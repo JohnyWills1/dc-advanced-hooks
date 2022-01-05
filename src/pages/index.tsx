00b1b7bf851b16e59f4a6b8b75f50eb0e1632ba3
Jonathan Wills <jonathan_wills@icloud.com>
2022-01-05 19:25:01 +0900

    💄 Add basic structure of title and logo to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react"
 import styled from "styled-components"
 
-const IndexPage = () => <Wrapper></Wrapper>
+const IndexPage = () => (
+  <Wrapper>
+    <Logo src="/images/logos/react-logo.svg" alt="react logo" />
+    <Title>Build a web app with React Hooks</Title>
+    <Caption>20 sections - 3 hours of videos</Caption>
+  </Wrapper>
+)
 
 export default IndexPage
 
@@ -9,3 +15,14 @@ const Wrapper = styled.div`
   height: 1247px;
   background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
 `
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`
+
+const Title = styled.h1``
+
+const Caption = styled.p``
+
+const Description = styled.p``
